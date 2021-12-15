f4788b92c9b0c579d0ee06391abbc0c40e113e4a
Daria V <voronovadaria@gmail.com>
2021-12-15 20:01:37 +0300

    ManyToMany [#331986]

--- src/main/java/ru/job4j/hibernate/model/HbmRun.java
+++ src/main/java/ru/job4j/hibernate/model/HbmRun.java
@@ -2,24 +2,51 @@ package ru.job4j.hibernate.model;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class HbmRun {
 
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+    private final SessionFactory sessionFactory = new MetadataSources(registry)
+            .buildMetadata()
+            .buildSessionFactory();
+
     public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
-        try {
-            SessionFactory sf = new MetadataSources(registry)
-                    .buildMetadata()
-                    .buildSessionFactory();
-            Session session = sf.openSession();
-            session.beginTransaction();
+        new HbmRun().runExampleManyToMany();
+    }
+
+    private void runExampleManyToMany() {
+        executeTransaction(session -> {
+            Author author1 = Author.of("Neil Gaiman");
+            Author author2 = Author.of("Terry Pratchett");
+            Author author3 = Author.of("Samantha van Leer");
+            Book book1 = Book.of("Good Omens");
+            Book book2 = Book.of("The Sandman: Book of Dreams");
+            Book book3 = Book.of("Between the Lines");
+            Book book4 = Book.of("Discworld");
+            author1.addBook(book1);
+            author1.addBook(book2);
+            author2.addBook(book1);
+            author2.addBook(book4);
+            author3.addBook(book3);
+            session.persist(author1);
+            session.persist(author2);
+            session.persist(author3);
+            Author author1InDb = session.find(Author.class, 1);
+            session.remove(author1InDb);
+        });
+    }
+
+    private void runExampleOneToMany() {
+        executeTransaction(session -> {
             List<CarModel> models = List.of(
                     CarModel.of("Spark"),
                     CarModel.of("Tahoe"),
@@ -30,12 +57,22 @@ public class HbmRun {
             CarBrand brand = CarBrand.of("Chevrolet");
             models.forEach(brand::addCarModel);
             session.save(brand);
-            session.getTransaction().commit();
-            session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        });
+    }
+
+    private void executeTransaction(final Consumer<Session> command) {
+        final Session session = sessionFactory.openSession();
+        final Transaction transaction = session.beginTransaction();
+        try {
+            command.accept(session);
+            transaction.commit();
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
         } finally {
-            StandardServiceRegistryBuilder.destroy(registry);
+            session.close();
         }
     }
 }
+
+
